c84fe62b06072ddc5489e22dfeb8b6a022aa94a1
guangjin2.xian <guangjin2.xian@zhaopin.com.cn>
2023-05-20 21:00:48 +0800

    feat: 1. update plugin

--- main.js
+++ main.js
@@ -1,29 +1,4 @@
-function init_data(source_lang, target_lang) {
-    return {
-        "source": "",
-        "detect": true,
-        "os_type": "ios",
-        "device_id": "F1F902F7-1780-4C88-848D-71F35D88A602",
-        "trans_type": 'auto2' + target_lang,
-        "media": "text",
-        "request_id": 424238335,
-        "user_id": "",
-        "dict": true
-    };
-}
-
-function getRandomNumber() {
-    const rand = Math.floor(Math.random() * 99999) + 100000;
-    return rand * 1000;
-}
-
-
-// function main(text, contextText, completion) {
-//     (async () => {
-//         console.log("main" + text + contextText)
-//         completion("hello")
-//     })()
-// }
+const {getRandomNumber, init_data} = require("./utils");
 
 function main(text, contextText, completion) {
     (async () => {
