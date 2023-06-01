66ce594db9ebe601a8fc6ed2e18363ee599154e4
guangjin2.xian <guangjin2.xian@zhaopin.com.cn>
2023-06-01 21:47:56 +0800

    feat: 1. 优化

--- main.js
+++ main.js
@@ -1,21 +1,4 @@
-function init_data(source_lang, target_lang) {
-    return {
-        "source": "",
-        "detect": true,
-        "os_type": "ios",
-        "device_id": "F1F902F7-1780-4C88-848D-71F35D88A602",
-        "trans_type": 'auto2' + target_lang,
-        "media": "text",
-        "request_id": 424238335,
-        "user_id": "",
-        "dict": true
-    };
-}
-
-function getRandomNumber() {
-    const rand = Math.floor(Math.random() * 99999) + 100000;
-    return rand * 1000;
-}
+var utils = require('./utils.js');
 
 function main(text, contextText, completion) {
     (async () => {
@@ -35,11 +18,11 @@ function main(text, contextText, completion) {
                 target_lang = 'zh'
             }
 
-            id = getRandomNumber()
+            id = utils.getRandomNumber()
             const url = 'https://interpreter.cyapi.cn/v1/translator';
-            const post_data = init_data(source_lang, target_lang)
+            const post_data = utils.init_data(source_lang, target_lang)
             post_data.source = translate_text
-            post_data.request_id = getRandomNumber()
+            post_data.request_id = utils.getRandomNumber()
             try {
                 let response = await fetch(
                     url,
